eee8a19f5c74e231eb57062027919c9b21fa92ff
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-17 00:00:19 +0700

    feat: zerolog main + add seed to main

--- go.mod
+++ go.mod
@@ -8,4 +8,9 @@ require (
 	github.com/g8rswimmer/go-twitter/v2 v2.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
 	"os"
 
+	"github.com/rs/zerolog/log"
+
 	"github.com/bookpanda/snapping-thoughts/src/client"
+	seed "github.com/bookpanda/snapping-thoughts/src/seeds"
 	"github.com/joho/godotenv"
 )
 
+func handleArgs(db *client.DynamoDBClient) {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) >= 1 {
+		switch args[0] {
+		case "seed":
+			err := seed.Execute(db, args[1:]...)
+			if err != nil {
+				log.Fatal().
+					Str("service", "seeder").
+					Msg("Not found seed")
+			}
+			os.Exit(0)
+		}
+	}
+}
+
 func loadEnv() {
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal().Msg("Error loading .env file")
 	}
 }
 
@@ -27,10 +48,11 @@ func main() {
 
 	tableName := os.Getenv("TABLE_NAME")
 	dynamoClient := client.NewDynamoDBClient(tableName)
+	handleArgs(dynamoClient)
 
 	item, err := dynamoClient.GetItem()
 	if err != nil {
-		log.Panicf("get item error: %v", err)
+		log.Error().Str("dynamoClient", "get item error").Err(err)
 	}
 	if item == nil {
 		return
@@ -38,17 +60,17 @@ func main() {
 
 	err = dynamoClient.UpdateItem(item.Id)
 	if err != nil {
-		log.Panicf("update item error: %v", err)
+		log.Error().Str("dynamoClient", "update item error").Err(err)
 	}
 
 	tweetResponse, err := twitterClient.CreateTweet(item.Message)
 	if err != nil {
-		log.Panicf("create tweet error: %v", err)
+		log.Error().Str("twitterClient", "create tweet error").Err(err)
 	}
 
 	enc, err := json.MarshalIndent(tweetResponse, "", "    ")
 	if err != nil {
-		log.Panic(err)
+		log.Error().Str("twitterClient", "marshal response error").Err(err)
 	}
 	fmt.Println(string(enc))
 }
