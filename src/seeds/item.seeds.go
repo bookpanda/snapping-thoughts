03adcc0fd4f4b4b968d4e7598909f74b50a67924
Idhibhat Pankam <marginpankam@gmail.com>
2023-12-17 01:01:27 +0700

    feat: add data

--- src/seeds/item.seeds.go
+++ src/seeds/item.seeds.go
@@ -6,11 +6,383 @@ var items = []item.Item{
 	{
 		Id:      "1",
 		IsUsed:  "no",
-		Message: "Hello World",
+		Message: "If I weigh £99 and eat a pound of nachos, am I 1% nacho?",
 	},
 	{
 		Id:      "2",
 		IsUsed:  "no",
-		Message: "ss",
+		Message: "What if you drop soap on the floor? Is the floor clean or is the soap dirty?",
+	},
+	{
+		Id:      "3",
+		IsUsed:  "no",
+		Message: "Which orange came first? The color or the fruit?",
+	}, {
+		Id:      "4",
+		IsUsed:  "no",
+		Message: "If two vegans are arguing, is it still considered beef?",
+	}, {
+		Id:      "5",
+		IsUsed:  "no",
+		Message: "If you were born deaf, what language would you think?",
+	}, {
+		Id:      "6",
+		IsUsed:  "no",
+		Message: "If you get out of the shower clean? How does your towel get dirty?",
+	}, {
+		Id:      "7",
+		IsUsed:  "no",
+		Message: "If Apple made a car, would it still have Windows?",
+	}, {
+		Id:      "8",
+		IsUsed:  "no",
+		Message: "When we yawn, do deaf people think we're screaming?",
+	}, {
+		Id:      "9",
+		IsUsed:  "no",
+		Message: "If you're waiting for the waiter, aren't you the waiter?",
+	}, {
+		Id:      "10",
+		IsUsed:  "no",
+		Message: "Why aren't iphone chargers just called Apple juice?",
+	}, {
+		Id:      "11",
+		IsUsed:  "no",
+		Message: "If you work as security at a Samsung store, does that make you Guardian of the Galaxy?",
+	}, {
+		Id:      "12",
+		IsUsed:  "no",
+		Message: "How do you throw away a garbage can?",
+	}, {
+		Id:      "13",
+		IsUsed:  "no",
+		Message: "If you buy a bigger bed, you're left with more bedroom but less bedroom.",
+	}, {
+		Id:      "14",
+		IsUsed:  "no",
+		Message: "Have you ever felt bugs on you when there are no bugs on you? They're the ghosts of the bugs you've killed.",
+	}, {
+		Id:      "15",
+		IsUsed:  "no",
+		Message: "When you clean a vacuum cleaner, aren't you the vacuum cleaner?",
+	}, {
+		Id:      "16",
+		IsUsed:  "no",
+		Message: "Nothing is ever really on fire but rather fire is on things.",
+	}, {
+		Id:      "17",
+		IsUsed:  "no",
+		Message: "They said if life is unfair to everyone. Does that mean life is actually fair?",
+	}, {
+		Id:      "18",
+		IsUsed:  "no",
+		Message: "If I get scared half to death twice, what then?",
+	}, {
+		Id:      "19",
+		IsUsed:  "no",
+		Message: "Why is it called taking a dump when you're leaving it?",
+	}, {
+		Id:      "20",
+		IsUsed:  "no",
+		Message: "Kind of funny how being down for something and up for something mean the same thing.",
+	}, {
+		Id:      "21",
+		IsUsed:  "no",
+		Message: "If you're in the living room and you pass away. Did you die or you just knocked out?",
+	}, {
+		Id:      "22",
+		IsUsed:  "no",
+		Message: "Why is the pizza box a square if the pizza is a circle and the slice is a triangle?",
+	},
+	{
+		Id:      "23",
+		IsUsed:  "no",
+		Message: "Yo, why is it called a building if it's already built?",
+	}, {
+		Id:      "24",
+		IsUsed:  "no",
+		Message: "How does a sponge hold water if it's full of holes?",
+	}, {
+		Id:      "25",
+		IsUsed:  "no",
+		Message: "The blinks of your eyes get removed from your memory",
+	}, {
+		Id:      "26",
+		IsUsed:  "no",
+		Message: "What would happen if Pinocchio said my nose will grow now?",
+	}, {
+		Id:      "27",
+		IsUsed:  "no",
+		Message: "People who need glasses just got bad graphics.",
+	}, {
+		Id:      "28",
+		IsUsed:  "no",
+		Message: "Why is bacon called bacon and cookies called cookies if you cook bacon and bake cookies?",
+	}, {
+		Id:      "29",
+		IsUsed:  "no",
+		Message: "Do clothes in China just say made down the road?",
+	}, {
+		Id:      "30",
+		IsUsed:  "no",
+		Message: "If your shirt isn't tucked into your pants, is your pants tucked into your shirt?",
+	}, {
+		Id:      "31",
+		IsUsed:  "no",
+		Message: "If you're invisible and you close your eyes, can you see through your eyelids?",
+	}, {
+		Id:      "32",
+		IsUsed:  "no",
+		Message: "A fire truck is actually a water truck.",
+	}, {
+		Id:      "33",
+		IsUsed:  "no",
+		Message: "Why are deliveries on a ship called cargo but in a car it's called a shipment?",
+	}, {
+		Id:      "34",
+		IsUsed:  "no",
+		Message: "If one teacher can't teach all subjects, why is one child expected to study all subjects?",
+	}, {
+		Id:      "35",
+		IsUsed:  "no",
+		Message: "Are oranges named oranges because oranges are orange or is orange named orange because oranges are orange?",
+	}, {
+		Id:      "36",
+		IsUsed:  "no",
+		Message: "What happens to the car if you press the brake and the accelerator at the same time? Does it take a screenshot?",
+	}, {
+		Id:      "37",
+		IsUsed:  "no",
+		Message: "The youngest picture of you is also the oldest picture of you.",
+	}, {
+		Id:      "38",
+		IsUsed:  "no",
+		Message: "If we have watermelon, shouldn't we also have fire melon, earth melon and air melon?",
+	}, {
+		Id:      "39",
+		IsUsed:  "no",
+		Message: "Why do we drive in parkways, but park in driveways.",
+	}, {
+		Id:      "40",
+		IsUsed:  "no",
+		Message: "You know, if you think about it, your burps are just your pukes' farts.",
+	}, {
+		Id:      "41",
+		IsUsed:  "no",
+		Message: "If it rains on a Sunday, does that mean it's now a rain day?",
+	}, {
+		Id:      "42",
+		IsUsed:  "no",
+		Message: "Your alarm sound is technically your theme song since it plays at the start of every episode.",
+	}, {
+		Id:      "43",
+		IsUsed:  "no",
+		Message: "Clapping is just hitting yourself repeatedly because you like something.",
+	}, {
+		Id:      "44",
+		IsUsed:  "no",
+		Message: "The more I light my lighter, the lighter my lighter gets until it's too light to light",
+	}, {
+		Id:      "45",
+		IsUsed:  "no",
+		Message: "Do you realize that history classes are only going to get longer and more difficult as time goes on?",
+	}, {
+		Id:      "46",
+		IsUsed:  "no",
+		Message: "What if every country in the world has ninjas? But we only know about Japan's ninjas because they're so bad at being ninjas.",
+	}, {
+		Id:      "47",
+		IsUsed:  "no",
+		Message: "Why is there a 'd' in fridge but not in refrigerator?",
+	}, {
+		Id:      "48",
+		IsUsed:  "no",
+		Message: "My guy, every book you've ever read is just a remix of the dictionary, right?",
+	}, {
+		Id:      "49",
+		IsUsed:  "no",
+		Message: "If you stab a cereal box, are you a 'serial' killer?",
+	}, {
+		Id:      "50",
+		IsUsed:  "no",
+		Message: "When you're talking, you can't breathe out through your nose.",
+	}, {
+		Id:      "51",
+		IsUsed:  "no",
+		Message: "Neil A. Armstrong was the first person to step on the moon. Neil A. spelled backwards is alien.",
+	}, {
+		Id:      "52",
+		IsUsed:  "no",
+		Message: "You don't need a parachute to skydive. You only need a parachute to skydive twice.",
+	}, {
+		Id:      "53",
+		IsUsed:  "no",
+		Message: "Mirrors don't break. They only multiply.",
+	}, {
+		Id:      "54",
+		IsUsed:  "no",
+		Message: "Ever realize that humans cut down birdhouses to make more birdhouses?",
+	}, {
+		Id:      "55",
+		IsUsed:  "no",
+		Message: "If two mind readers are reading each other's minds, whose mind are they reading?",
+	}, {
+		Id:      "56",
+		IsUsed:  "no",
+		Message: "Your fingers have fingertips but your toes don't have toe tips, yet you can tiptoe but not tipfinger.",
+	}, {
+		Id:      "57",
+		IsUsed:  "no",
+		Message: "If 'tomb' is pronounced 'toom' and 'womb' is pronounced 'woom', why isn't 'bomb' pronounced 'boom'?",
+	}, {
+		Id:      "58",
+		IsUsed:  "no",
+		Message: "How can you replicate the feeling of falling from high altitudes in a nightmare if you've never fallen like that before?",
+	}, {
+		Id:      "59",
+		IsUsed:  "no",
+		Message: "Ice Cubes float in a pool of their own blood.",
+	}, {
+		Id:      "60",
+		IsUsed:  "no",
+		Message: "If you pour ice water on a hot dog, does it become a chill dog?",
+	}, {
+		Id:      "61",
+		IsUsed:  "no",
+		Message: "You technically have two minutes to live but every time you breathe it restarts the timer.",
+	}, {
+		Id:      "62",
+		IsUsed:  "no",
+		Message: "The word 'nun' is just the letter 'n' doing a cartwheel.",
+	}, {
+		Id:      "63",
+		IsUsed:  "no",
+		Message: "Taxes are just a yearly subscription to the country you live in, childhood is the free trial.",
+	}, {
+		Id:      "64",
+		IsUsed:  "no",
+		Message: "If your parents always told you not to take candy from strangers, then what is Halloween?",
+	}, {
+		Id:      "65",
+		IsUsed:  "no",
+		Message: "Your eraser slowly dies because of your mistakes.",
+	}, {
+		Id:      "66",
+		IsUsed:  "no",
+		Message: "The number of people older than you will never increase.",
+	}, {
+		Id:      "67",
+		IsUsed:  "no",
+		Message: "If a fly loses its wings. Is it now called a walk?",
+	}, {
+		Id:      "68",
+		IsUsed:  "no",
+		Message: "Would Lightning McQueen get car insurance or life insurance?",
+	}, {
+		Id:      "69",
+		IsUsed:  "no",
+		Message: "If you sweat in a sweater, does that make you the sweater?",
+	}, {
+		Id:      "70",
+		IsUsed:  "no",
+		Message: "How come your lips don't touch when you say 'touch' but they do when you say 'separate'?",
+	},
+	{
+		Id:      "71",
+		IsUsed:  "no",
+		Message: "If earth is the third planet from the sun, doesn't that mean that every country is a third world country?",
+	}, {
+		Id:      "72",
+		IsUsed:  "no",
+		Message: "Light bulbs were such a good idea they became the symbol for a good idea.",
+	}, {
+		Id:      "73",
+		IsUsed:  "no",
+		Message: "Lawyers hope you get sued. Doctors hope you get sick. Cops hope you're a criminal. Mechanics hope you have car trouble. But, only a thief wishes you prosperity.",
+	}, {
+		Id:      "74",
+		IsUsed:  "no",
+		Message: "They say the asteroid killed all the dinosaurs. You could also say it killed many birds with one stone.",
+	}, {
+		Id:      "75",
+		IsUsed:  "no",
+		Message: "Reading a book is looking at a dead tree and hallucinating.",
+	}, {
+		Id:      "76",
+		IsUsed:  "no",
+		Message: "Let me ask you something, why when you say 'forwards' or 'back' your lips move in that direction",
+	}, {
+		Id:      "77",
+		IsUsed:  "no",
+		Message: "Popcorn is explosions frozen in time that you can eat.",
+	}, {
+		Id:      "78",
+		IsUsed:  "no",
+		Message: "Cold toilet seats are physically uncomfortable, warm toilet seats are mentally uncomfortable.",
+	}, {
+		Id:      "79",
+		IsUsed:  "no",
+		Message: "Have you ever noticed that water is simply a drink where the temperature determines the taste.",
+	}, {
+		Id:      "80",
+		IsUsed:  "no",
+		Message: "'I'm sorry' and 'I apologize' have the exact same meaning except when you're at a funeral.",
+	}, {
+		Id:      "81",
+		IsUsed:  "no",
+		Message: "Two wrongs don't make a right but three lefts do.",
+	}, {
+		Id:      "82",
+		IsUsed:  "no",
+		Message: "When you ask for someone's name, you're just asking what kind of noise you should make to get their attention.",
+	}, {
+		Id:      "83",
+		IsUsed:  "no",
+		Message: "Why does natural spring water that has been flowing for hundreds of years have an expiration date?",
+	},
+	{
+		Id:      "84",
+		IsUsed:  "no",
+		Message: "If you took care of chickens, wouldn't that make you a chicken tender?",
+	}, {
+		Id:      "85",
+		IsUsed:  "no",
+		Message: "Why does your nose run but your feet smell.",
+	}, {
+		Id:      "86",
+		IsUsed:  "no",
+		Message: "Did you know if you take the value of pi and hold it up to a mirror, it actually spells 'pie'.",
+	}, {
+		Id:      "87",
+		IsUsed:  "no",
+		Message: "Water is just a portal to a universe where we can fly but can't breathe.",
+	}, {
+		Id:      "88",
+		IsUsed:  "no",
+		Message: "If snails are so slow, why do we never see them coming? It's like, boom, there's a snail.",
+	}, {
+		Id:      "89",
+		IsUsed:  "no",
+		Message: "Mosquitoes use blood to make their eggs, that means you're one third their father.",
+	}, {
+		Id:      "90",
+		IsUsed:  "no",
+		Message: "When a pregnant woman goes swimming, she becomes a human submarine.",
+	}, {
+		Id:      "91",
+		IsUsed:  "no",
+		Message: "Are bugs born knowing they can walk up walls or do they just accidentally do it one day and go check me out.",
+	}, {
+		Id:      "92",
+		IsUsed:  "no",
+		Message: "National anthems can be considered country music.",
+	}, {
+		Id:      "93",
+		IsUsed:  "no",
+		Message: "Your head slowly 3D prints your hair.",
+	}, {
+		Id:      "94",
+		IsUsed:  "no",
+		Message: "Bullets only do their job after they're fired.",
 	},
 }
